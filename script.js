b129169d968694a379378edca88fe8ca0ed7bd50
A K M Saiful Islam <shubro42@yahoo.com>
2021-01-30 15:37:39 +0600

    unnecessary code deleted

--- script.js
+++ script.js
@@ -35,19 +35,17 @@ function handleTicketPrice(ticket, isIncrease) {
         newTicketNumber = currentTicketNumber + 1;
     }
 
-    if (isIncrease == false && currentTicketNumber >0){
+    if (isIncrease == false && currentTicketNumber > 0) {
         newTicketNumber = currentTicketNumber - 1;
-}
+    }
 
     document.getElementById(ticket + '-ticket-number').value = newTicketNumber;
 
     let ticketPrice = 0;
 
-    if(ticket == 'f-class'){
+    if (ticket == 'f-class') {
         ticketPrice = newTicketNumber * 150;
-    }
-
-    else if(ticket == 'e-class'){
+    } else if (ticket == 'e-class') {
         ticketPrice = newTicketNumber * 100;
     }
 
@@ -56,17 +54,17 @@ function handleTicketPrice(ticket, isIncrease) {
 }
 
 //Total Price
-function calculateTotal(){
+function calculateTotal() {
     const fClassTicket = document.getElementById('f-class-ticket-number');
     const fClassTicketNumber = parseInt(fClassTicket.value);
 
     const eClassTicket = document.getElementById('e-class-ticket-number');
     const eClassTicketNumber = parseInt(eClassTicket.value);
 
-    const totalTicketPrice = fClassTicketNumber*150 + eClassTicketNumber*100;
+    const totalTicketPrice = fClassTicketNumber * 150 + eClassTicketNumber * 100;
     document.getElementById('price-subtotal').innerText = totalTicketPrice;
 
-    const totalVat = 0.1*totalTicketPrice;
+    const totalVat = 0.1 * totalTicketPrice;
     document.getElementById('price-vat').innerText = totalVat;
 
     const priceTotal = totalTicketPrice + totalVat;
@@ -81,59 +79,11 @@ function calculateTotal(){
 document.getElementById('book-now-button').addEventListener('click', function () {
     document.getElementById('booking-input').style.display = 'none';
     document.getElementById('booking-confirmation').style.display = 'block';
-    
+
     document.getElementById('confirmation-flying-from').value = document.getElementById('flying-from').value;
     document.getElementById('confirmation-flying-to').value = document.getElementById('flying-to').value;
     document.getElementById('confirmation-departure').value = document.getElementById('departure').value;
     document.getElementById('confirmation-return').value = document.getElementById('return').value;
 })
 
-
-
-//////////////////////////////
-////For future reference:////
-////////////////////////////
-
-// calculate ticket price without function
-
-// document.getElementById('e-class-minus').addEventListener('click', function () {
-//     let currentEclassTicketNumber = document.getElementById('e-class-ticket-number').value;
-//     currentEclassTicketNumber = parseInt(currentEclassTicketNumber);
-
-//     let newEclassTicketNumber = currentEclassTicketNumber;
-
-//     if (currentEclassTicketNumber > 0) {
-//         newEclassTicketNumber = currentEclassTicketNumber - 1;
-//     }
-//     document.getElementById('e-class-ticket-number').value = newEclassTicketNumber;
-
-//     const eClassPrice = newEclassTicketNumber * 100;
-//     console.log(eClassPrice);
-// })
-
-///-------------------------------------------------
-
-
-// Single function for first class ticket
-
-
-// function handleFclassTicketPrice(isIncrease) {
-//     let currentFclassTicketNumber = document.getElementById('f-class-ticket-number').value;
-//     currentFclassTicketNumber = parseInt(currentFclassTicketNumber);
-
-//     let newFclassTicketNumber = currentFclassTicketNumber;
-
-//     if (isIncrease == true) {
-
-//         newFclassTicketNumber = currentFclassTicketNumber + 1;
-//     }
-
-//     if (isIncrease == false && currentFclassTicketNumber >0){
-//             newFclassTicketNumber = currentFclassTicketNumber - 1;
-//     }
-
-//     document.getElementById('f-class-ticket-number').value = newFclassTicketNumber;
-
-//     const fClassPrice = newFclassTicketNumber * 150;
-//     console.log(fClassPrice);
-// }
+//End - Thank you
